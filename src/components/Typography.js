4bbff8fce4508382817946086e2e59252165bf2b
Joan Wolkerstorfer <joanwolk@gmail.com>
2024-06-21 11:46:56 -0700

    remove un-hyphenated typography aliases

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -1,14 +1,10 @@
 import { createElement } from 'react';
 
 export const TypographyElements = {
-  "body1": "p",
   "body-1": "p",
-  "body2": "p",
   "body-2": "p",
   "span": "span",
-  "h1small": "h1",
   "h1-small": "h1",
-  "h1large": "h1",
   "h1-large": "h1",
   "h2": "h2",
   "h3": "h3",
@@ -21,15 +17,7 @@ export default function Typography(
     throw TypeError("Unsupported element type for Typography component")
   };
 
-  if (elType === "body1" || elType === "body-1") {
-    className = className + " body-1";
-  } else if (elType === "body2" || elType === "body-2") {
-    className = className + " body-2";
-  } else if (elType === "h1large" || elType === "h1-large") {
-    className = className + " h1-large";
-  }
-
-  className = className + " typography";
+  className = elType + " " + className + " typography";
 
   return createElement(
     TypographyElements[elType],
