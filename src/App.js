cd11d1f5a231238397f424391013cdd95006b3c2
Joan Wolkerstorfer <joanwolk@gmail.com>
2024-06-21 15:58:18 -0700

    add responsive font size on h1 with demo text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Typography elType='h3'>
+        <Typography elType='h1-large'>
           Edit <code>src/App.js</code> and save to reload.
         </Typography>
 
