923bd2f1d8a008d38e92fe0e7842dcda4db42602
Poulami Saha <31151391+poulami-saha@users.noreply.github.com>
2024-06-22 18:43:16 +0200

    [KAN-3]:Adding Reusable Button component (#2)
    * Adding reusable Button component
    
    * Colors added to variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/utility/Button/Button";
 
 function App() {
   return (
@@ -17,6 +18,46 @@ function App() {
         >
           Learn React
         </a>
+        <h3>Primary Button</h3>
+        <div className="buttonContainer buttonContainerColor">
+          <Button
+            type="primary"
+            onClick={() => alert("Primary Button Clicked")}
+          >
+            Primary Button
+          </Button>
+
+          <Button type="primary" disabled>
+            Primary Button
+          </Button>
+        </div>
+
+        <h3>Secondary Button</h3>
+        <div className="buttonContainer buttonContainerColor">
+          <Button
+            type="secondary"
+            onClick={() => alert("Secondary Button Clicked")}
+          >
+            Secondary Button
+          </Button>
+
+          <Button type="secondary" disabled>
+            Secondary Button
+          </Button>
+        </div>
+
+        <h3>Tertiary Button</h3>
+        <div className="buttonContainer">
+          <Button
+            type="tertiary"
+            onClick={() => alert("Tertiary Button Clicked")}
+          >
+            Tertiary Button
+          </Button>
+          <Button type="tertiary" disabled>
+            Tertiary Button
+          </Button>
+        </div>
       </header>
     </div>
   );
