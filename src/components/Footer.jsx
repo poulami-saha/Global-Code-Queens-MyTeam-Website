b195007828f3cb6c2bdd05cd6d425ecc9033ad87
keziabaidoo <keziabaidoo100@gmail.com>
2024-06-22 16:36:05 +0000

    branch name changed to responsive-footer-component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 
-function footer() {
+function Footer() {
     return (
         <div>
             
@@ -10,4 +10,4 @@ function footer() {
     )
 }
 
-export default footer
+export default Footer;
