febad65923e496a031a0af5cc3637bf094002983
keziabaidoo <keziabaidoo100@gmail.com>
2024-06-25 12:58:44 +0000

    added text content to HeroFooter component and some module css. Footer component also updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.css'; 
+import HeroFooter from './HeroFooter';
+
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>2024 My Company</p>
+       <HeroFooter/>
     </footer>
   );
 };
