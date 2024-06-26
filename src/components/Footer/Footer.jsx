6f0dde697e700f199b8df8a26c47638e2b2092e9
keziabaidoo <56594873+keziabaidoo@users.noreply.github.com>
2024-06-26 18:28:04 +0000

    Revert "added text content to HeroFooter component and some module css. Foote…" (#10)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import styles from './Footer.module.css'; 
-import HeroFooter from './HeroFooter';
-
-
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-       <HeroFooter/>
+      <p>2024 My Company</p>
     </footer>
   );
 };
