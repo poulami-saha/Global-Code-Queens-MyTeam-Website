1034c747c1e3a2c00dc3fe361d24691f790d5b49
Sehaj Wadhwa <104042287+sehajwadhwa@users.noreply.github.com>
2024-06-28 17:42:57 -0400

    js setup 2 section

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
-import styles from './Home.module.css';  
+import React from "react";
+import styles from "./Home.module.css";
+import Typography from "../../utility/Typography/Typography";
+import pattern3 from "../../../assets/bg-pattern-home-3.svg";
+import iconcog from "../../../assets/icon-cog.svg";
+import iconchart from "../../../assets/icon-chart.svg";
+import iconperson from "../../../assets/icon-person.svg";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="Home">
+      Home
+      <div className="Home__secondsection">
+        <div className="Home__secondsection__firstContainer">
+          <Typography elType="h2">
+            Build & manage distributed teams like no one else.
+          </Typography>
+        </div>
+        <div className="Home__secondsection__secondContainer">
+          <div className="Home__secondsection__secondContainer__first">
+            <img src={iconperson} alt="icon-person" />
+            <Typography elType="body1">Experienced Individuals</Typography>
+            <Typography elType="body2">
+              Our network is made up of highly experienced professionals who are
+              passionate about what they do.
+            </Typography>
+          </div>
+          <div className="Home__secondsection__secondContainer__second">
+            <img src={iconcog} alt="icon-cog" />
+            <Typography elType="body1">Easy to Implement</Typography>
+            <Typography elType="body2">
+              Our processes have been refined over years of implementation
+              meaning our teams always deliver.
+            </Typography>
+          </div>
+          <div className="Home__secondsection__secondContainer__third">
+            <img src={iconchart} alt="icon-chart" />
+            <Typography elType="body1">Enhanced Productivity</Typography>
+            <Typography elType="body2">
+              Our customized platform with in-built analytics helps you manage
+              your distributed teams.
+            </Typography>
+          </div>
+        </div>
+      </div>
+      <img src={pattern3} alt="pattern-3" />
+    </section>
   );
 };
 
