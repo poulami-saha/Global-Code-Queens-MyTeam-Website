b59ad984ab3fa5dca5f5189088139c4fb01605d7
Sehaj Wadhwa <104042287+sehajwadhwa@users.noreply.github.com>
2024-06-29 20:50:17 -0400

    mobile view designed

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -8,42 +8,45 @@ import iconperson from "../../../assets/icon-person.svg";
 
 const Home = () => {
   return (
-    <section className="Home">
-      Home
-      <div className="Home__secondsection">
-        <div className="Home__secondsection__firstContainer">
-          <Typography elType="h2">
-            Build & manage distributed teams like no one else.
-          </Typography>
+    <section className={styles.Home2}>
+      <div className={styles.Home__secondsection}>
+        <div className={styles.Home__secondsection__firstContainer}>
+          <div className={styles.Home2__Heading}>
+            <Typography elType="h2">
+              Build & manage distributed teams like no one else.
+            </Typography>
+          </div>
+          <div className={styles.Home__pattern}>
+            <img src={pattern3} alt="pattern-3" />
+          </div>
         </div>
-        <div className="Home__secondsection__secondContainer">
-          <div className="Home__secondsection__secondContainer__first">
+        <div className={styles.Home__secondsection__secondContainer}>
+          <div className={styles.Home__secondsection__secondContainer__first}>
             <img src={iconperson} alt="icon-person" />
-            <Typography elType="body1">Experienced Individuals</Typography>
-            <Typography elType="body2">
+            <Typography elType="body1" className={styles.Home2__body1}>Experienced Individuals</Typography>
+            <Typography elType="body2" className={styles.Home2__body2}>
               Our network is made up of highly experienced professionals who are
               passionate about what they do.
             </Typography>
           </div>
-          <div className="Home__secondsection__secondContainer__second">
+          <div className={styles.Home__secondsection__secondContainer__second}>
             <img src={iconcog} alt="icon-cog" />
-            <Typography elType="body1">Easy to Implement</Typography>
-            <Typography elType="body2">
+            <Typography elType="body1" className={styles.Home2__body1}>Easy to Implement</Typography>
+            <Typography elType="body2" className={styles.Home2__body2}>
               Our processes have been refined over years of implementation
               meaning our teams always deliver.
             </Typography>
           </div>
-          <div className="Home__secondsection__secondContainer__third">
+          <div className={styles.Home__secondsection__secondContainer__third}>
             <img src={iconchart} alt="icon-chart" />
-            <Typography elType="body1">Enhanced Productivity</Typography>
-            <Typography elType="body2">
+            <Typography elType="body1" className={styles.Home2__body1}>Enhanced Productivity</Typography>
+            <Typography elType="body2" className={styles.Home2__body2}>
               Our customized platform with in-built analytics helps you manage
               your distributed teams.
             </Typography>
           </div>
         </div>
       </div>
-      <img src={pattern3} alt="pattern-3" />
     </section>
   );
 };
