52653fc3e938c81b4a7fad36a330c5e6d3951ad6
Poulami Saha <saha.poulami91@gmail.com>
2024-06-30 22:26:08 +0200

    About Details added

--- src/components/About/AboutDetails.jsx
+++ src/components/About/AboutDetails.jsx
@@ -4,22 +4,21 @@ import containerImage from "../../assets/bg-pattern-about-1-mobile-nav-1.svg";
 const AboutDetails = () => {
   return (
     <section className={styles.container}>
+      <img
+        src={containerImage}
+        alt="background-image"
+        className={styles.containerImage}
+      />
       <Typography elType="h1Small">About</Typography>
       <article>
-        <div></div>
-        <Typography elType="body1">
+        <div className={styles.bar}></div>
+        <Typography elType="body1" className={styles.aboutus}>
           We help companies build dynamic teams made up of top global talent.
           Using our network of passionate professionals we drive innovation and
           deliver incredible outcomes. Talented, diverse teams shape the best
           products and experiences. We’ll bring those teams to you.
         </Typography>
       </article>
-
-      <img
-        src={containerImage}
-        alt="background-image"
-        className={styles.containerImage}
-      />
     </section>
   );
 };
