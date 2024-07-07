37c95c245dd1d99590762f04bb756f50a948d5bf
Poulami Saha <saha.poulami91@gmail.com>
2024-07-07 22:32:24 +0200

    Addressed review comments

--- src/components/About/AboutDetails.jsx
+++ src/components/About/AboutDetails.jsx
@@ -5,9 +5,9 @@ const AboutDetails = () => {
   return (
     <section className={styles.container}>
       <Typography elType="h1Small">About</Typography>
-      <article>
+      <article className={styles.aboutus}>
         <div className={styles.bar}></div>
-        <Typography elType="body1" className={styles.aboutus}>
+        <Typography elType="body1">
           We help companies build dynamic teams made up of top global talent.
           Using our network of passionate professionals we drive innovation and
           deliver incredible outcomes. Talented, diverse teams shape the best
