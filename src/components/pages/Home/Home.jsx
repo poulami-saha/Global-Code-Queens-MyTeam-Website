d77052b425fa0c8f214923b4be5dd2ba548a35ac
bwilli07 <berika.williams@tufts.edu>
2024-07-08 12:47:56 -0400

    added Talent to home page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 // import styles from './Home.module.css';
+import Talent from "../../HomeContainer/Talent/Talent";
 import SuccessStories from "../../HomeContainer/SuceessStories/SuccessStories";
 
 const Home = () => {
   return <>
+      <Talent />
       <SuccessStories />
       </>;
 };
