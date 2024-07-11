8f3e04033de912fc7e0d60b6c8f59e1a2f1ad9bf
Uzma Khan <urbazoha@gmail.com>
2024-07-11 21:41:42 +0500

    Created responsive-header-component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Header.module.css';  
+import React, { useState } from "react";
+// import { Link } from 'react-router-dom';
+import icon_close from "../../assets/icon-close.svg";
+import logo from "../../assets/logo.svg";
+import styles from "./Header.module.css";
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) :
+   setMobileMenu(true);
+  };
   return (
     <header className={styles.header}>
+      <img src={logo} alt="myteam logo" className={styles.logo} />
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
+        <ul className={ mobileMenu ? '' :  styles['hide-mobile-menu']}>
+          <img src={icon_close} alt="" className={styles.icon_close} onClick={toggleMenu} />
+         <li>home</li>
+          <li>about</li>
         </ul>
+          <button className={styles.btn}>contact us</button>
       </nav>
+    
     </header>
   );
 };
