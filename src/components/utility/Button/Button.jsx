24065e4a56949604bfbfae8a55928d2752cefb0c
Joan Wolkerstorfer <joanwolk@gmail.com>
2024-07-19 17:02:53 -0500

    set button colors in CSS, not HTML style property

--- src/components/utility/Button/Button.jsx
+++ src/components/utility/Button/Button.jsx
@@ -3,64 +3,22 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import styles from "./Button.module.css";
 
-const buttonColorStyle = {
-  primary: {
-    white: {
-      color: "#fff",
-      borderColor: "#fff",
-      hoverBackgroundColor: "white",
-      hoverColor: "#002529",
-    },
-    dark: {
-      color: "#012F34",
-      borderColor: "#012F34",
-      hoverBackgroundColor: "#012F34",
-      hoverColor: "#fff",
-    },
-    showMore: {
-      color: "#012F34",
-      backgroundColor: "#f67e7e",
-      borderColor: "#012F34",
-      hoverBackgroundColor: "#79c8c7",
-      hoverColor: "#012F34",
-    },
-    hideMore: {
-      color: "#012F34",
-      backgroundColor: "#79c8c7",
-      borderColor: "#012F34",
-      hoverBackgroundColor: "#f67e7e",
-      hoverColor: "#012F34",
-    },
-  },
-  secondary: {
-    color: "#002529",
-    hoverBackgroundColor: "#79C8C7",
-    hoverColor: "#002529",
-  },
-};
-
 const Button = ({ type = "primary", elType, disabled = false, onClick, children, className }) => {
   const isSecondary = type === "secondary";
   const buttonClass = classNames(styles.button, className, {
     [styles.disabled]: disabled,
     [styles.buttonSecondary]: isSecondary,
-  });
-
-  const buttonStyle = isSecondary ? buttonColorStyle.secondary : buttonColorStyle.primary[elType];
-  const colorConfig = {
-    "--button-color": buttonStyle.color,
-    "--button-background-color": buttonStyle.backgroundColor,
-    "--button-border-color": buttonStyle.borderColor,
-    "--button-hover-background-color": buttonStyle.hoverBackgroundColor,
-    "--button-hover-color": buttonStyle.hoverColor,
-  };
+    [styles.buttonPrimaryDark]: type === "primary" && elType === "dark",
+    [styles.buttonPrimaryWhite]: type ==="primary" && elType === "white",
+    [styles.buttonShowMore]: elType === "showMore",
+    [styles.buttonHideMore]: elType === "hideMore",
+  })
 
   return (
     <button
       className={buttonClass}
       onClick={disabled ? null : onClick}
       disabled={disabled}
-      style={colorConfig}
     >
       {children}
     </button>
@@ -68,7 +26,7 @@ const Button = ({ type = "primary", elType, disabled = false, onClick, children,
 };
 
 Button.propTypes = {
-  elType: PropTypes.oneOf(["white", "dark"]),
+  elType: PropTypes.oneOf(["white", "dark", "showMore", "hideMore"]),
   type: PropTypes.oneOf(["primary", "secondary"]),
   disabled: PropTypes.bool,
   onClick: PropTypes.func,
