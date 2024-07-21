4cd052b342dd77193c3ffe22e3683123afed8f96
Rika Williams <103834650+berikawilliams@users.noreply.github.com>
2024-07-21 06:36:24 -0400

    [KAN-11] home first section (#22)
    * Created desktop version of home 1st section
    
    * intial commit for home-first-section feature
    
    * added Talent to home page
    
    * added responsiveness to styles
    
    * Update src/components/HomeContainer/Talent/Talent.module.css
    
    Accepted changes to min-width for media query for desktop to 960px.
    
    Co-authored-by: Poulami Saha <31151391+poulami-saha@users.noreply.github.com>
    
    * Updating the lock file
    
    * added .heading style to typography element and removed extra div
    
    * added class to span element
    
    * add consistent pattern to position background-images
    
    * added missing parenthesis
    
    * updated npm version
    
    ---------
    
    Co-authored-by: bwilli07 <berika.williams@tufts.edu>
    Co-authored-by: Poulami Saha <saha.poulami91@gmail.com>
    Co-authored-by: Poulami Saha <31151391+poulami-saha@users.noreply.github.com>

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Talent from "../../HomeContainer/Talent/Talent";
 import SuccessStories from "../../HomeContainer/SuceessStories/SuccessStories";
 import BuildManagement from "../../HomeContainer/Management/BuildManagement";
 
 const Home = () => {
   return (
     <>
+      <Talent />
       <BuildManagement />
       <SuccessStories />
     </>
