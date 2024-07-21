c83f0004cb0274cf954840ff7fc3fedf663215fd
Joan Wolkerstorfer <joanwolk@gmail.com>
2024-07-21 11:29:40 -0500

    refactor HTML and CSS of ReadyToStart - use background image to simplify image positioning - separate section style div from content display/positioning div - use CSS variables for color - use margins on .mainContent to make responsive transitions smoother and remove explicit height declarations

--- src/components/ReadyToStart/ReadyToStart.jsx
+++ src/components/ReadyToStart/ReadyToStart.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styles from "./ReadyToStart.module.css";
-import mainImage from "../../assets/bg-pattern-home-6-about-5.svg";
 import Button from "../utility/Button/Button.jsx";
 import Typography from "../utility/Typography/Typography.js";
 
 function ReadyToStart() {
   return (
-    <div className={styles.mainContent}>
-      <Typography elType="h2" className={styles.text}>
-        Ready to get started?
-      </Typography>
+    <div className={styles.ReadyToStart}>
+      <div className={styles.mainContent}>
+        <Typography elType="h2" className={styles.text}>
+          Ready to get started?
+        </Typography>
 
-      <Button elType="primaryDark">contact us</Button>
-      <img src={mainImage} alt="" className={styles.mainImg} />
+        <Button elType="primaryDark">contact us</Button>
+      </div>
     </div>
   );
 }
