3b2d905d750a56300d26199d3664215d68e7e05f
Joan Wolkerstorfer <joanwolk@gmail.com>
2024-08-02 22:03:15 -0700

    refactor Clients layout and update snapshot (#47)
    - wrap h2 and news in `.body` div
    - use background image properties instead of ::after
    - fix img sizes
    - fix div widths
    - use container query to change flex justification when all logos can fit on one line

--- src/components/AboutContainer/Clients/Clients.jsx
+++ src/components/AboutContainer/Clients/Clients.jsx
@@ -10,27 +10,29 @@ import gadgetsNow from "../../../assets/logo-gadgets-now.png";
 const Clients = () => {
   return (
     <section className={styles.container}>
-      <Typography elType="h2" className={styles.title}>
-        Some of our clients
-      </Typography>
-      <div className={styles.news}>
-        <img src={verge} alt="The Verge" className={styles.logoVerge} />
-        <img
-          src={jakarta}
-          alt="TheJakarta Post"
-          className={styles.logoJakarta}
-        />
-        <img
-          src={guardian}
-          alt="The Guardian"
-          className={styles.logoGuardian}
-        />
-        <img src={techRadar} alt="techRadar" className={styles.logoTechRadar} />
-        <img
-          src={gadgetsNow}
-          alt="Gadgets Now"
-          className={styles.logoGadgets}
-        />
+      <div className={styles.body}>
+        <Typography elType="h2" className={styles.title}>
+          Some of our clients
+        </Typography>
+        <div className={styles.news}>
+          <img src={verge} alt="The Verge" className={styles.logoVerge} />
+          <img
+            src={jakarta}
+            alt="The Jakarta Post"
+            className={styles.logoJakarta}
+          />
+          <img
+            src={guardian}
+            alt="The Guardian"
+            className={styles.logoGuardian}
+          />
+          <img src={techRadar} alt="Tech Radar" className={styles.logoTechRadar} />
+          <img
+            src={gadgetsNow}
+            alt="Gadgets Now"
+            className={styles.logoGadgets}
+          />
+        </div>
       </div>
     </section>
   );
